003f78880191a51d8ffe0a13ef4009d4817cd2a0
Edgar Rios Navarro <edgar.gs@gmail.com>
2019-04-16 08:47:51 -0500

    Add API hello

--- Dockerfile
+++ Dockerfile
@@ -5,5 +5,6 @@ WORKDIR $APP_HOME
 COPY . .
 
 RUN pip install Flask gunicorn
+RUN pip install flask_restful
 
 CMD exec gunicorn --bind :$PORT --workers 1 --threads 8 app:app
--- app.py
+++ app.py
@@ -1,13 +1,21 @@
 import os
 
 from flask import Flask
+from flask_restful import Resource, Api
 
 app = Flask(__name__)
+api = Api(app)
 
 @app.route('/')
 def hello_world():
     target =os.environ.get('TARGET','World')
     return 'Hello {} from Cloud Run!\n'.format(target)
+
+class Hello(Resource):
+    def get(self, name):
+    return {"Hello":name}
+
+api.add_resource(Hello, '/hello/<name>')
 
 if __name__ == "__main__":
     app.run(debug=True,host='0.0.0.0',port=int(os.environ.get('PORT',8080)))
